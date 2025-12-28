2d71a927865aeddc2c5649c4b22e03cba39eca32
VadimSel <selyaaa33@gmail.com>
2025-12-28 17:53:43 +0300

    fix

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,36 +2,7 @@ import type { ApiAreasTypes, ApiResultsTypes } from '../types/apiTypes';
 
 const BASE = '/c300/api/v4/test/';
 
-// export const api = {
-//   getMeters: async (): Promise<ApiResultsTypes> => {
-//     const res = await fetch(`${BASE}meters/?limit=20&offset=20`, {
-//       method: 'GET',
-//     });
-//     const data = await res.json();
-//     return data;
-//   },
-
-//   getAreas: async (id: string): Promise<ApiAreasTypes> => {
-//     const res = await fetch(`${BASE}areas/?id=${id}`, {
-//       method: "GET",
-//     })
-//     const data = await res.json()
-//     return data.results[0];
-//   }
-// };
-
 export const api = {
-  // getMeters: async (
-  //   limit: number,
-  //   offset: number
-  // ): Promise<ApiResultsTypes> => {
-  //   const res = await fetch(
-  //     `${BASE}meters/?limit=${limit}&offset=${offset}`,
-  //     { method: 'GET' }
-  //   );
-  //   return res.json();
-  // },
-
   getMeters: async (
     limit: number,
     offset: number
